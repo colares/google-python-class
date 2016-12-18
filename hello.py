34a74f756c6462f3a68697b326e4ae48f2f7fea3
Thiago Colares <thiago@agilize.com.br>
2016-12-18 13:56:53 -0200

    Primeiro commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python -tt
+# coding=UTF-8
+
+import sys
+
+# Define a main() function that prints a little greeting
+def main():
+	print "Hello"
+
+'''This is the standard boilerplate that calls the main() function.
+# Caso voce execute o programa usando python ou ./, esse if e true e o main e excutado
+# Se você só quiser incluir esste arquivo em outro código, como um módulo, sem eexecutá-lo automaticamente, esse if vira false a função não é executada
+# Esse código é um boilerplate pq vc pode usar em todo arquivo q quiser q haja assim
+'''
+if __name__ == '__main__':
+	main()
